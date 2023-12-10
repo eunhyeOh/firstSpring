12da3692928d06494cfe712a35c4b34868ffda8d
eunhyeOh <oheunhye95@gmail.com>
2023-12-10 15:24:53 +0900

    회원 웹 기능 - 등록

--- src/main/java/grace/firstspring/controller/MemberController.java
+++ src/main/java/grace/firstspring/controller/MemberController.java
@@ -1,23 +1,14 @@
 package grace.firstspring.controller;
 
+import grace.firstspring.domain.Member;
 import grace.firstspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
-
-    /**필드 주입
-    @Autowired private MemberService;
-    /*
-
-    /**세터 주입
-    private MemberService memberService;
-    @Autowired
-    public void SetMemberService(MemberService memberService){
-        this.memberService = memberService;
-    }
-    */
 
     //생성자주입
     private MemberService memberService;
@@ -25,5 +16,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    /**
+     * 회원가입
+     * */
+    @GetMapping("/member/new")
+    public String createFrom(){
+        return "members/createMemberForm";
+    }
+
+    /**
+     * 회원가입 / 등록버튼
+     * */
+    @PostMapping("members/new")
+    public String create(MemberForm from){
+
+        Member member = new Member();
+        member.setName(from.GetName());
+
+        memberService.Join(member);
+
+        //홈화면으로 보내기
+        return "redirect:/";
     }
 }
---
+++ src/main/java/grace/firstspring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package grace.firstspring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String GetName(){
+        return name;
+    }
+
+    public void SetName(String name){
+        this.name = name;
+    }
+}
