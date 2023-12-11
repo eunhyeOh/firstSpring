1f9c5ae4101a00cef423ec001a7e07a3d12a3fe9
eunhyeOh <oheunhye95@gmail.com>
2023-12-11 22:44:46 +0900

    회원 웹 기능 - 등록(수정) & 조회

--- src/main/java/grace/firstspring/controller/MemberController.java
+++ src/main/java/grace/firstspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import grace.firstspring.domain.Member;
 import grace.firstspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,15 +32,23 @@ public class MemberController {
     /**
      * 회원가입 / 등록버튼
      * */
-    @PostMapping("members/new")
-    public String create(MemberForm from){
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
 
         Member member = new Member();
-        member.setName(from.GetName());
+        member.setName(form.getName());
 
         memberService.Join(member);
 
         //홈화면으로 보내기
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
--- src/main/java/grace/firstspring/controller/MemberForm.java
+++ src/main/java/grace/firstspring/controller/MemberForm.java
@@ -3,11 +3,11 @@ package grace.firstspring.controller;
 public class MemberForm {
     private String name;
 
-    public String GetName(){
-        return name;
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void SetName(String name){
-        this.name = name;
+    public String getName(){
+        return name;
     }
 }
