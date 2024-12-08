ed5b4aced267bb7b83c8eb0d33aa7c3d586d724f
eunhyeOh <oheunhye95@gmail.com>
2024-12-08 12:54:53 +0900

    RestAPI CRUD

---
+++ src/main/java/grace/firstspring/repository/JdbcMemberRepository.java
@@ -0,0 +1,60 @@
+package grace.firstspring.repository;
+
+import grace.firstspring.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate();
+
+        }catch (){
+
+        }
+
+
+
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
